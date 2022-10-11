a6a613a204c2e87b655834b329b99ad7aae6f5bb
ousseini <adnanejiko@gmail.com>
2022-10-11 22:15:16 +0000

    Initial commit

---
+++ src/main/java/com/supdeci/album_photo_jfx_rmi/HelloController.java
@@ -0,0 +1,14 @@
+package com.supdeci.album_photo_jfx_rmi;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
